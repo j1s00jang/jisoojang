7b2da5135c3198c6c2f9f02878e57b3c81cd8c02
j1s00jang <jjang87@my.bcit.ca>
2026-01-29 09:47:28 -0800

    Home: Remove import code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,3 @@
-import "./Home.css";
-import jisoosImage from "../assets/main/jisoos.png";
-import portfolioImage from "../assets/main/portfolio.png";
-import scaffoldLogo from "../assets/main/scaffold_logo.png";
-import montroLogo from "../assets/main/montro_logo.png";
-import interactiveThumbnail from "../assets/main/interactive_thumbnail.png";
-import folderImage from "../assets/main/folder_image.png";
-
 function Home() {
     return (
         <div className="home-container">
